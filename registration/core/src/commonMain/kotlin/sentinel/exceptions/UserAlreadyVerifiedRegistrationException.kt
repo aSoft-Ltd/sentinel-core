7d5e5c6dc41bb657fbdd9b500a05f48ee2729f0d
andylamax <andylamax@programmer.net>
2023-11-26 12:05:11 +0300

    sunday afternoon sync

---
+++ registration/core/src/commonMain/kotlin/sentinel/exceptions/UserAlreadyVerifiedRegistrationException.kt
@@ -0,0 +1,9 @@
+package sentinel.exceptions
+
+import kase.Status
+
+class UserAlreadyVerifiedRegistrationException(
+    val email: String
+) : RegistrationException("User with email ($email) has already been verified registration") {
+    override val status by lazy { Status.CODE_409_Conflict }
+}
